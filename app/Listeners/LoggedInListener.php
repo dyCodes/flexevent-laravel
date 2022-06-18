f4e2d9dd514c6484d5fabbaf752b5c416cf9e53b
dyCodes <yusufdaudu51@gmail.com>
2022-06-18 02:11:41 +0100

    Add last-logged-in feature

---
+++ app/Listeners/LoggedInListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use Carbon\Carbon;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LoggedInListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        // Update last_logged_in field
+        $user =  $event->user;
+        $user->last_logged_in = Carbon::now()->format('Y-m-d H:i:s');
+        $user->save();
+    }
+}
