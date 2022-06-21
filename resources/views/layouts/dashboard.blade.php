4fff7ac5c563c3b6bad20bc4fb322514eaec6c72
dyCodes <yusufdaudu51@gmail.com>
2022-06-21 23:17:41 +0100

    Update dashboard view

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,110 +11,24 @@
   <!-- Favicons -->
   <link href="/assets/img/favicon.png" rel="icon" />
   <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-
   <!-- CSS Files -->
   <link rel="stylesheet" href="/assets/modules/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/modules/fontawesome/css/all.min.css">
   <link rel="stylesheet" href="/assets/modules/jquery-selectric/selectric.css">
-
   <!-- Template CSS -->
   <link rel="stylesheet" href="/assets/admin/style.css">
   <link rel="stylesheet" href="/assets/admin/components.css">
   <link rel="stylesheet" href="/assets/admin/custom.css">
+  {{-- Include custom page files in head --}}
+  @yield('head')
 </head>
 
 <body>
   <div id="app">
     <div class="main-wrapper main-wrapper-1">
-      <nav class="navbar navbar-expand-lg main-navbar bg-primary">
-        <ul class="navbar-nav w-100 justify-content-between">
-          <li>
-            <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
-          </li>
-
-          <li class="dropdown"><a href="#" data-toggle="dropdown"
-              class="nav-link dropdown-toggle nav-link-lg nav-link-user" aria-expanded="false">
-              <img alt="image" src="/assets/admin/avatar/avatar-4.png" class="rounded-circle mr-1">
-              <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right">
-              <div class="dropdown-title">
-                Logged in {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', auth()->user()->last_logged_in)->diffForHumans() }}
-              </div>
-              <a href="features-profile.html" class="dropdown-item has-icon">
-                <i class="far fa-user"></i> Profile
-              </a>
-              <a href="features-settings.html" class="dropdown-item has-icon">
-                <i class="fas fa-cog"></i> Settings
-              </a>
-              <div class="dropdown-divider"></div>
-              <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
-                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                <i class="fas fa-sign-out-alt"></i> Logout
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf
-                </form>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </nav>
-
-      <div class="main-sidebar sidebar-style-2">
-        <aside id="sidebar-wrapper">
-          <div class="sidebar-brand mb-4">
-            <a href="{{ route('dashboard') }}">
-              <img src="/assets/img/logo.png" class="sLogo" alt="{{ config('app.name') }}">
-            </a>
-          </div>
-
-          <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
-
-            <li class="active">
-              <a href="{{ route('dashboard') }}" class="nav-link" data-toggle="tooltip"
-                data-original-title="Dashboard"><i class="fas fa-fire"></i>
-                <span>Dashboard</span>
-              </a>
-            </li>
-
-            <li><a class="nav-link" href="{{ route('upload') }}" data-toggle="tooltip"
-                data-original-title="Upload">
-                <i class="far fa-square"></i> <span>Upload Images</span></a>
-            </li>
-
-            <li><a class="nav-link" href="{{ route('gallery') }}" data-toggle="tooltip"
-                data-original-title="gallery">
-                <i class="far fa-square"></i> <span>Gallery</span></a>
-            </li>
-
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-columns"></i>
-                <span>Portfolio</span></a>
-              <ul class="dropdown-menu" style="display: none;">
-                <li><a class="nav-link" href="layout-transparent.html">View Portfolio</a></li>
-                <li><a class="nav-link" href="layout-default.html">Add Photo</a></li>
-              </ul>
-            </li>
-
-            <li><a class="nav-link" href="#" data-toggle="tooltip" data-original-title="Testimonials">
-                <i class="far fa-square"></i> <span>Testimonials</span></a>
-            </li>
-
-            <li><a class="nav-link" href="#" data-toggle="tooltip" data-original-title="Settings">
-                <i class="fas fa-cog"></i> <span>Settings</span></a>
-            </li>
-          </ul>
-
-          {{-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="{{ route('logout') }}" class="btn btn-danger btn-lg btn-block btn-icon-split"
-              onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-              <i class="fas fa-sign-out-alt"></i> Log Out
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
-          </div> --}}
-
-        </aside>
-      </div>
+      <header>
+        @include('layouts.navigation')
+      </header>
 
       <!-- Main Content -->
       @yield('content')
@@ -124,6 +38,8 @@
           Copyright &copy; 2022 <div class="bullet"></div> FlexEvent</a>
         </div>
       </footer>
+
+      <div id="page_overlay"></div>
     </div>
   </div>
 
@@ -135,9 +51,20 @@
   <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
   <script src="/assets/modules/jquery-selectric/jquery.selectric.min.js"></script>
 
+  {{-- Include custom page files in footer --}}
+  @yield('footer')
+
   <!-- Template JS File -->
   <script src="/assets/admin/scripts.js"></script>
-  {{-- <script src="/assets/admin/custom.js"></script> --}}
+  <script>
+    // Prevent multiple form submit
+    $(function() {
+      // Add overlay on form submit
+      $('#app form').on('submit', function() {
+        $('#page_overlay').addClass('show');
+      });
+    });
+  </script>
 </body>
 
 </html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,96 @@
+@php
+function is_active_page($page)
+{
+    return Request::is($page) ? 'active' : '';
+}
+@endphp
+
+<nav class="navbar navbar-expand-lg main-navbar bg-primary">
+  <ul class="navbar-nav w-100 justify-content-between">
+    <li>
+      <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
+    </li>
+
+    <li class="dropdown">
+      <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user"
+        aria-expanded="false">
+        <img alt="image" src="/assets/admin/avatar/avatar-4.png" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right">
+        <div class="dropdown-title">
+          Logged in
+          {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', auth()->user()->last_logged_in)->diffForHumans() }}
+        </div>
+
+        {{-- <a href="features-profile" class="dropdown-item has-icon">
+          <i class="far fa-user"></i> Profile
+        </a> --}}
+
+        <a href="features-settings" class="dropdown-item has-icon">
+          <i class="fas fa-cog"></i> Settings
+        </a>
+        <div class="dropdown-divider"></div>
+        <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+          onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+          <i class="fas fa-sign-out-alt"></i> Logout
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf
+          </form>
+        </a>
+      </div>
+    </li>
+  </ul>
+</nav>
+
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand mb-4">
+      <a href="{{ route('dashboard') }}">
+        <img src="/assets/img/logo.png" class="sLogo" alt="{{ config('app.name') }}">
+      </a>
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Dashboard</li>
+
+      <li class="{{ is_active_page('admin_dashboard') }}">
+        <a href="{{ route('dashboard') }}" class="nav-link" data-toggle="tooltip" data-original-title="Dashboard"><i
+            class="fas fa-fire"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+
+      <li class="{{ is_active_page('admin/upload') }}">
+        <a class="nav-link" href="{{ route('upload') }}" data-toggle="tooltip" data-original-title="Upload">
+          <i class="fas fa-upload"></i> <span>Upload Images</span></a>
+      </li>
+
+      <li class="{{ is_active_page('admin/gallery') }}">
+        <a class="nav-link" href="{{ route('gallery') }}" data-toggle="tooltip" data-original-title="gallery">
+          <i class="far fa-images"></i> <span>Gallery</span></a>
+      </li>
+
+      <li class="dropdown {{ is_active_page('admin/testimonials') }}">
+        <a href="#" class="nav-link has-dropdown"><i class="far fa-comments"></i>
+          <span>Testimonials</span></a>
+
+        <ul class="dropdown-menu" style="display: none;">
+          <li><a class="nav-link" href="{{ route('testimonials') }}">View Testimonials</a></li>
+          <li><a class="nav-link" href="{{ route('testimonials.create') }}">Add New</a></li>
+        </ul>
+      </li>
+
+      <li class="{{ is_active_page('admin/settings') }}">
+        <a class="nav-link" href="#" data-toggle="tooltip" data-original-title="Settings">
+          <i class="fas fa-cog"></i> <span>Settings</span></a>
+      </li>
+
+      <li>
+        <a class="nav-link" href="{{ route('logout') }}" data-toggle="tooltip" data-original-title="Logout"
+          onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+          <i class="fas fa-sign-out-alt"></i> <span>Logout</span>
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+      </li>
+    </ul>
+  </aside>
+</div>
