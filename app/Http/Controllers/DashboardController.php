a742b964ebc1db6c07ff82b9e1d5d9277e032d93
dyCodes <yusufdaudu51@gmail.com>
2022-07-11 20:45:47 +0100

    Update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
+use App\Models\Service;
+use App\Models\Testimonial;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+
+use function PHPUnit\Framework\returnSelf;
 
 class DashboardController extends Controller
 {
-    //
     public function index()
     {
-        return view('dashboard.index');
+        // Get/Store cache values
+        $images = Cache::remember('images', now()->addDay(), function () {
+            return Image::FetchByAlbum('gallery');
+        });
+        $testimonials = Cache::remember('testimonials', now()->addDay(), function () {
+            return Testimonial::latest()->get();
+        });
+        $services = Cache::get('services');
+
+        $data =  array(
+            'images' => $images->take(7),
+            'testimonials' => $testimonials->take(4),
+            'image_count' => $images->count(),
+            'testimonial_count' =>  $testimonials->count(),
+            'service_count' => $services ? $services->count() : Service::count(),
+        );
+
+        return view('dashboard.index')->with($data);
     }
+
     // Override fortify default logout
     public function logout(Request $request)
     {
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,14 +11,14 @@
           <div class="col-lg-4 col-md-6 col-sm-12">
             <div class="card card-statistic-2">
               <div class="card-icon shadow-primary bg-primary">
-                <i class="fas fa-users"></i>
+                <i class="fas fa-clone"></i>
               </div>
               <div class="card-wrap">
                 <div class="card-header">
-                  <h4>Total Visitors</h4>
+                  <h4>Services</h4>
                 </div>
                 <div class="card-body">
-                  59
+                  {{ $service_count }}
                 </div>
               </div>
             </div>
@@ -33,7 +33,7 @@
                 <div class="card-header">
                   <h4>Testimonials</h4>
                 </div>
-                <div class="card-body">3</div>
+                <div class="card-body">{{ $testimonial_count }}</div>
               </div>
             </div>
           </div>
@@ -45,21 +45,22 @@
               </div>
               <div class="card-wrap">
                 <div class="card-header">
-                  <h4>Gallery Images</h4>
+                  <h4>Images</h4>
                 </div>
-                <div class="card-body">8</div>
+                <div class="card-body">{{ $image_count }}</div>
               </div>
             </div>
           </div>
         </div>
 
         <div class="row">
-          <div class="col-md-12">
+          <div class="col-12 col-md-12 col-lg-8">
             <div class="card">
               <div class="card-header">
                 <h4>Testimonials</h4>
                 <div class="card-header-action">
-                  <a href="#" class="btn btn-primary">View More <i class="fas fa-chevron-right"></i></a>
+                  <a href="{{ route('testimonials') }}" class="btn btn-primary">Manage <i
+                      class="fas fa-chevron-right"></i></a>
                 </div>
               </div>
 
@@ -68,60 +69,63 @@
                   <table class="table table-striped">
                     <tbody>
                       <tr>
-                        <th>Invoice ID</th>
-                        <th>Customer</th>
-                        <th>Status</th>
-                        <th>Due Date</th>
+                        <th>#</th>
+                        <th>Client</th>
+                        <th>Content</th>
                       </tr>
-                      <tr>
-                        <td><a href="#">INV-87239</a></td>
-                        <td class="font-weight-600">Kusnadi</td>
-                        <td>
-                          <div class="badge badge-warning">Unpaid</div>
-                        </td>
-                        <td>July 19, 2018</td>
 
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-48574</a></td>
-                        <td class="font-weight-600">Hasan Basri</td>
-                        <td>
-                          <div class="badge badge-success">Paid</div>
-                        </td>
-                        <td>July 21, 2018</td>
-
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-76824</a></td>
-                        <td class="font-weight-600">Muhamad Nuruzzaki</td>
-                        <td>
-                          <div class="badge badge-warning">Unpaid</div>
-                        </td>
-                        <td>July 22, 2018</td>
-
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-84990</a></td>
-                        <td class="font-weight-600">Agung Ardiansyah</td>
-                        <td>
-                          <div class="badge badge-warning">Unpaid</div>
-                        </td>
-                        <td>July 22, 2018</td>
-
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-87320</a></td>
-                        <td class="font-weight-600">Ardian Rahardiansyah</td>
-                        <td>
-                          <div class="badge badge-success">Paid</div>
-                        </td>
-                        <td>July 28, 2018</td>
-
-                      </tr>
+                      @if (count($testimonials))
+                        @php $x = 1; @endphp
+                        @foreach ($testimonials as $item)
+                          {{-- GALLERY ITEM --}}
+                          <tr>
+                            <td>{{ $x++ }}</td>
+                            <td class="font-weight-600">{{ $item->client_name }}</td>
+                            <td>
+                              {{ $item->getExcerpt(100) }}
+                            </td>
+                          </tr>
+                        @endforeach
+                      @else
+                        <tr>
+                          <td colspan="3" class="text-center font-weight-600">No Testimonials Found!</td>
+                        </tr>
+                      @endif
                     </tbody>
                   </table>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-header">
+                <h4>Gallery</h4>
+                <div class="card-header-action">
+                  <a href="{{ route('gallery') }}" class="btn btn-primary">Manage <i
+                      class="fas fa-chevron-right"></i></a>
+                </div>
+              </div>
+
+              <div class="card-body">
+                @if (count($images) > 0)
+                  <div class="gallery gallery-md">
+                    @foreach ($images as $image)
+                      <div class="gallery-item" data-image="{{ url($image->url) }}"></div>
+                    @endforeach
+                    {{-- Show more images count --}}
+                    @if ($image_count - $images->count())
+                      <div class="gallery-item gallery-more">
+                        <div>+{{ $image_count - $images->count() }}</div>
+                      </div>
+                    @endif
+                  </div>
+                @else
+                  <h6 class="text-center p-3">No Image Found!</h6>
+                @endif
+              </div>
+
             </div>
           </div>
         </div>
